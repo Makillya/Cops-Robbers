c378420753c610c06b9f00ef1cd6d9807fb4df7a
Nicole Zurita <npz2dw@gmail.com>
2017-05-11 13:09:34 -0700

    added second pin - same color as first

--- CopsNRobbers/ViewController.swift
+++ CopsNRobbers/ViewController.swift
@@ -22,10 +22,16 @@ class ViewController: UIViewController {
         let mapSpan = MKCoordinateSpanMake(0.01, 0.01)
         let mapRegion = MKCoordinateRegionMake(centerLocation, mapSpan)
         self.map.setRegion(mapRegion, animated: true)
+        
         let myAnnotation: MKPointAnnotation = MKPointAnnotation()
         myAnnotation.coordinate = CLLocationCoordinate2DMake(47.6098342, -122.1967169)
         myAnnotation.title = "Current location"
         self.map.addAnnotation(myAnnotation)
+        
+        let myAnnotation2: MKPointAnnotation = MKPointAnnotation()
+        myAnnotation2.coordinate = CLLocationCoordinate2DMake(47.6111106, -122.1980925)
+        myAnnotation2.title = "Other location"
+        self.map.addAnnotation(myAnnotation2)
 		// Do any additional setup after loading the view, typically from a nib.
 	}
 
