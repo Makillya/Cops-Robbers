cbecabcb01615fd0f0bc9841a8344d03ced31250
Vineeth Raghunath <vin792@gmail.com>
2017-05-11 22:28:33 -0700

    added functionality to display other user locations on the map

--- CopsNRobbers/ViewController.swift
+++ CopsNRobbers/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 import MapKit
 import CoreLocation
 import Alamofire
+import Foundation
 
 class ViewController: UIViewController {
 
@@ -19,51 +20,86 @@ class ViewController: UIViewController {
     var locationManager: CLLocationManager?
     var userLocation: Array<Double> = [47.6098342,-122.1967169]
     var userAnnotation: MKPointAnnotation = MKPointAnnotation()
-    var users: Array<Any> = []
+    var userUUID: String = ""
+    var users = [String: Any]()
     
 	override func viewDidLoad() {
 		super.viewDidLoad()
         
         updateMap()
-//        let myAnnotation: MKPointAnnotation = MKPointAnnotation()
-//        myAnnotation.coordinate = CLLocationCoordinate2DMake(47.6098342, -122.1967169)
-//        myAnnotation.title = "Current location"
-//        self.map.addAnnotation(myAnnotation)
-//        
-//        let myAnnotation2: MKPointAnnotation = MKPointAnnotation()
-//        myAnnotation2.coordinate = CLLocationCoordinate2DMake(47.6111106, -122.1980925)
-//        myAnnotation2.title = "Other location"
-//        self.map.addAnnotation(myAnnotation2)
         
+        //Get UUID of device
+        if let uuid = UIDevice.current.identifierForVendor?.uuidString {
+            userUUID = uuid
+        }
         
-		// Do any additional setup after loading the view, typically from a nib.
-        
-        //Initialize Location Manager
+        //Initialize location manager
         locationManager = CLLocationManager()
         locationManager?.delegate = self
         locationManager?.desiredAccuracy = kCLLocationAccuracyBest
-        
         locationManager?.requestWhenInUseAuthorization()
         
-
+        //Retrieve all other user Locations
+        retrieveUsersLocationFromServer()
 	}
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
 	}
     
+    //Function which updates user's pin on the map
     func updateUserLocation() {
         userAnnotation.coordinate = CLLocationCoordinate2DMake(userLocation[0], userLocation[1])
-        userAnnotation.title = "User Location"
+        userAnnotation.title = "My Location"
         self.map.addAnnotation(userAnnotation)
     }
     
+    //Function which updates the center location of the map based on the user's current location
     func updateMap() {
-        let centerLocation = CLLocationCoordinate2DMake(userLocation[0], userLocation[1])
-        let mapSpan = MKCoordinateSpanMake(0.001, 0.001)
-        let mapRegion = MKCoordinateRegionMake(centerLocation, mapSpan)
-        self.map.setRegion(mapRegion, animated: true)
+        //let centerLocation = CLLocationCoordinate2DMake(userLocation[0], userLocation[1])
+        //let mapSpan = MKCoordinateSpanMake(1, 1)
+        //let mapRegion = MKCoordinateRegionMake(centerLocation, mapSpan)
+        //self.map.setRegion(mapRegion, animated: true)
+    }
+    
+    //Function which sends the user's current location to the server
+    func sendUserLocationDataToServer() {
+            let coordinates : Parameters = ["key" : [userUUID, userLocation[0], userLocation[1]]]
+            
+            Alamofire.request("http://52.36.124.53/sendData", method: .post, parameters: coordinates, encoding: URLEncoding.default).responseJSON {response in let _ = response.result.isSuccess}
+        
+    }
+    
+    //Function which retrieves all user locations from the server
+    func retrieveUsersLocationFromServer() {
+        Alamofire.request("http://52.36.124.53/getData").responseJSON { response in
+            if let JSON = response.result.value {
+                if let data = JSON as? [String: Any] {
+                    for (key, value) in data {
+                        let arr1 = value as! Array<Any>
+                        self.users[key] = arr1
+                    }
+                }
+                self.displayOtherUsersOnMap()
+            }
+        }
+    }
+    
+    //Function which displays all users locations on the map (other than the device location of the current user)
+    func displayOtherUsersOnMap() {
+        for (key, value) in users {
+            if key != userUUID {
+                var locationCoordinates: Array<Double> = []
+                for coordinate in value as! Array<String> {
+                    locationCoordinates.append(Double(coordinate)!)
+                }
+                
+                let otherUserAnnotation: MKPointAnnotation = MKPointAnnotation()
+                otherUserAnnotation.coordinate = CLLocationCoordinate2DMake(CLLocationDegrees(locationCoordinates[0]), CLLocationDegrees(locationCoordinates[1]))
+                otherUserAnnotation.title = key
+                self.map.addAnnotation(otherUserAnnotation)
+            }
+        }
     }
 }
 
@@ -76,17 +112,7 @@ extension ViewController: CLLocationManagerDelegate {
             userLocation[1] = currentLocation.longitude
             updateUserLocation()
             updateMap()
-            let uuid = UIDevice.current.identifierForVendor?.uuidString
-            let coordinates : Parameters = ["key" : [uuid!, userLocation[0], userLocation[1]]]
-            
-            Alamofire.request("http://52.36.124.53/sendData", method: .post, parameters: coordinates, encoding: URLEncoding.default).responseJSON {response in print()}
-            
-            
-            Alamofire.request("http://52.36.124.53/getData").responseJSON { response in
-                if let JSON = response.result.value {
-                    print(JSON)
-                }
-            }
+            sendUserLocationDataToServer()
         }
     }
     
@@ -96,5 +122,4 @@ extension ViewController: CLLocationManagerDelegate {
             locationManager?.allowsBackgroundLocationUpdates = true
         }
     }
-    
 }
